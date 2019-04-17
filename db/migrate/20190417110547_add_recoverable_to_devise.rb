4ba3f6b3cc29f4bd5b16eec89ebfaf7d84109d72
Alina Kovtun <alinakovtun11@gmail.com>
2019-04-17 16:40:10 +0300

    LVRUBY-83: Implement reset password

---
+++ db/migrate/20190417110547_add_recoverable_to_devise.rb
@@ -0,0 +1,9 @@
+class AddRecoverableToDevise < ActiveRecord::Migration[5.2]
+  def change
+    change_table :users do |u|
+      u.string   :reset_password_token
+      u.datetime :reset_password_sent_at
+    end
+    add_index :users, :reset_password_sent_at, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_13_131613) do
+ActiveRecord::Schema.define(version: 2019_04_17_110547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,11 @@ ActiveRecord::Schema.define(version: 2019_04_13_131613) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_sent_at"], name: "index_users_on_reset_password_sent_at", unique: true
   end
 
 end
