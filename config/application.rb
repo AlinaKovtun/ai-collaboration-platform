7b6059714893e20aefa5477c2f48f9ba4d002527
Alina Kovtun <alinakovtun11@gmail.com>
2019-04-18 20:11:52 +0300

    fixed rubocop

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module AiCollaborationPlatform
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
-# -- all .rb files in that directory are automatically loaded after loading
+    # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
     config.host = ENV['APP_HOST']
