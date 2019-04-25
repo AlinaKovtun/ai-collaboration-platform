29d094b98246c570a7301e6bffca1ce9411bf1cc
Alex Klymko <oleksandrklymko@gmail.com>
2019-04-25 14:18:56 +0300

    view and stylesheets fix

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,16 +6,10 @@ require 'factory_bot'
 RSpec.describe UsersController, type: :controller do
   let(:user) { create(:user) }
   let(:params) { { avatar: AvatarUploader.new(Rails.root.join('spec/test1.jpg')) } }
-  let(:params1) { { avatar: AvatarUploader.new(Rails.root.join('spec/test2.gif')) } }
   before { sign_in user }
   describe '#update' do
     it 'updates password whith valid params' do
       put :update, params: { id: user.id, user: params }
-      user.reload
-      expect(response).to redirect_to(user_path(user.id))
-    end
-    it 'update' do
-      put :update, params: { id: user.id, user: params1 }
       user.reload
       expect(response).to redirect_to(user_path(user.id))
     end
