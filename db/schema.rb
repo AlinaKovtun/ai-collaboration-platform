066ae920ea22b01bd7311d11ebebc402b76857ac
Alina Kovtun <alinakovtun11@gmail.com>
2019-05-02 13:56:53 +0300

    LVRUBY-19: add approved column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_130423) do
+ActiveRecord::Schema.define(version: 2019_05_02_102549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,16 +38,17 @@ ActiveRecord::Schema.define(version: 2019_04_22_130423) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.text "about_me"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.text "about_me"
     t.datetime "remember_created_at"
+    t.boolean "approved", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_sent_at"], name: "index_users_on_reset_password_sent_at", unique: true
