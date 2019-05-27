624b652e48d45bc45abd9ebdb4c3465acad48df7
Alina Kovtun <alinakovtun11@gmail.com>
2019-05-27 22:04:55 +0300

    Add roles helpers

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,4 +5,10 @@ class Role < ApplicationRecord
 
   validates :name, uniqueness: true
   scope :visible_to_users, -> { where.not(name: 'admin') }
+
+  %w[student expert mentor teacher admin].each do |role|
+    define_method "#{role}?" do
+      name == role
+    end
+  end
 end
