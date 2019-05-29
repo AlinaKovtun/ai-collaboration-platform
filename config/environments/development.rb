1442c88e520f6a8e1ff722b62fe3012c85948649
Alex Klymko <oleksandrklymko@gmail.com>
2019-05-29 22:58:24 +0300

    hound fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -70,5 +69,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
 end
