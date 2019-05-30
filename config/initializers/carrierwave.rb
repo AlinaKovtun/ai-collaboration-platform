03027ac675c18577b9aea1b07f32e9bcb1d681ff
Alex Klymko <oleksandrklymko@gmail.com>
2019-05-30 13:16:08 +0300

    ci fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,16 +5,16 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws' # required
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION'],
+    aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', 'test'),
+    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', 'test'),
+    region: ENV.fetch('AWS_REGION', 'test'),
     path_style: true
   }
   if Rails.env.test?
     config.storage = :file
   else
     config.storage = :fog
-    config.fog_directory  = ENV['AWS_BUCKET']
+    config.fog_directory  = ENV.fetch('AWS_BUCKET', 'test')
     config.fog_public     = false
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
