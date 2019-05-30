019e3282400f4c896eec61d7d83d7448f403b926
PidtserkovnyiVitalii <vitalii.pidtserkovnyi@gmail.com>
2019-05-30 23:16:43 +0300

    helper added

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module LinksHelper
   def links_helper(obj)
-    link_to obj.user.full_name, user_path(obj.user.id), class: "text-dark"
+    link_to obj.user.full_name, user_path(obj.user.id), class: 'text-dark'
   end
 end
