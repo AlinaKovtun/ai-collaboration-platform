d28a5f931d392b365b18038d3e4cbffa889d366c
Alex Klymko <oleksandrklymko@gmail.com>
2019-05-30 12:56:25 +0300

    Fixed rspec test

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,12 +3,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  if Rails.env.test? || Rails.env.cucumber?
-    storage :file
-  else
-    storage :fog
-  end
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'carrierwave/orm/activerecord'
-
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws' # required
   config.fog_credentials = {
@@ -11,7 +10,12 @@ CarrierWave.configure do |config|
     region: ENV['AWS_REGION'],
     path_style: true
   }
-  config.fog_directory  = ENV['AWS_BUCKET']
-  config.fog_public     = false
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  if Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_directory  = ENV['AWS_BUCKET']
+    config.fog_public     = false
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  end
 end
