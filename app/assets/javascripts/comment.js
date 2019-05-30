d5c2ad08b5d9e122d568ec6754f38b3e47532688
PidtserkovnyiVitalii <vitalii.pidtserkovnyi@gmail.com>
2019-05-30 20:48:19 +0300

    assets modified

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,19 +1,24 @@
 document.addEventListener('turbolinks:load', function(){
-  	$("button#add-reply").click(function(){
-      if ($(this).parent().find('form').find(".comment-edit-area").first().css("display") == "block") {
-        $(this).parent().find('form').find(".comment-edit-area").first().hide();
-        $(this).parent().find('form').find(".comment-edit-button").first().hide();
+    $("button#add-reply").click(function(){
+      thisComment = $(this).parent();
+      commentId = $(this).data('comment-id');
+      if (thisComment.find('#edit_area_'+commentId).css("display") == "block") {
+        thisComment.find("#edit_area_"+commentId).hide();
+        thisComment.find("#edit_button_"+commentId).hide();
       }
-    	$(this).parent().find('form').first().find(".reply-area").toggle();
-    	$(this).parent().find('form').first().find(".reply-button").toggle();
-  	});
-  	$("button#comment-edit").click(function(){
-      if ($(this).parent().find('form').first().find(".reply-area").css("display") == "block") {
-        $(this).parent().find('form').first().find(".reply-area").hide();
-        $(this).parent().find('form').first().find(".reply-button").hide();
+      thisComment.find("#reply_area_"+commentId).toggle();
+      thisComment.find("#reply_button_"+commentId).toggle();
+    });
+
+    $("button#comment-edit").click(function(){
+      commentId = $(this).data('comment-id');
+      thisComment = $(this).parent();
+      if (thisComment.find('#reply_area_'+commentId).css("display") == "block") {
+        thisComment.find("#reply_area_"+commentId).hide();
+        thisComment.find("#reply_button_"+commentId).hide();
       }
-    	$(this).parent().find('form').find(".comment-edit-area").first().toggle();
-    	$(this).parent().find('form').find(".comment-edit-button").first().toggle();
-  	});
+      thisComment.find("#edit_area_"+commentId).toggle();
+      thisComment.find("#edit_button_"+commentId).toggle();
+    });
   }
 );
