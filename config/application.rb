86a9fdda8fc21e7002916ca74584246cd4fb8742
nataliakozoriz <webkng@gmail.com>
2019-05-31 13:14:23 +0300

    set config

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
 module AiCollaborationPlatform
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
